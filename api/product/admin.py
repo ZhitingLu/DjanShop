237b4adff53306b9502220a8783a8dd1c96f47d9
Zhiting Lu <zhitinglu@outlook.com>
2023-07-20 08:25:42 +0200

    Register admin for Product

--- api/product/admin.py
+++ api/product/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 
+from . import models
+from .models import Product
 # Register your models here.
+
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'category', 'price', 'stock', 'is_active', 'image']
+    search_display = ['name', 'description']
