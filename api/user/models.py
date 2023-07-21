36ea03e6528a53d35bfae032fb1f3c10b1f8ca1b
Zhiting Lu <zhitinglu@outlook.com>
2023-07-21 09:37:31 +0200

    added model for custom user

--- api/user/models.py
+++ api/user/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 
-class User(AbstractUser):
+class CustomUser(AbstractUser):
     name = models.CharField(max_length=50, default="Anonymous")
     email = models.EmailField(max_length=254, unique=True)
     username = None
