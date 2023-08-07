fd6a4a164fdef6560352d8674dd3b339e90f261b
Zhiting Lu <zhitinglu@outlook.com>
2023-08-07 13:34:55 +0200

    adding oder in admin

--- api/order/admin.py
+++ api/order/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from . import models
 # Register your models here.
+
+
+@admin.register(models.Order)
+class CategoryAdmin(admin.ModelAdmin):
+    """
+    Sets how the model is shown in Django admin interface
+    """
+    list_display = ['id', 'user', ]
+    search_fields = ['id', 'user', ]
--- api/order/views.py
+++ api/order/views.py
@@ -16,3 +16,45 @@ def validate_user_session(id, token):
         return False
     except UserModel.DoesNotExist:
         return False
+
+
+
+@csrf_exempt
+def add(request, id, token):
+    if not validate_user_session(id, token):
+        return JsonResponse({'error': 'Please login',
+                             'code': '500'})
+    if request.method == 'POST':
+        user_id = id
+        transaction_id = request.POST['transaction_id']
+        amount = request.POST['amount']
+        products = request.POST['products']
+
+        total_pro = len(products.split(',')[:-1])
+
+        UserModel = get_user_model()
+
+        try:
+            user = UserModel.objects.get(pk=user_id)
+        except UserModel.DoesNotExist:
+            return JsonResponse({
+                'error': 'User does not exist'
+            })
+
+        order = Order(user=user,
+                      product_names=products,
+                      total_products=total_pro,
+                      transaction_id=transaction_id,
+                      total_amount=amount)
+        order.save()
+        return JsonResponse({
+            'success': True,
+            'error': False,
+            'message': 'Order placed successfully'
+        })
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all().order_by('-id')
+    serializer_class = OrderSerializer
+    
